a5336ee182ee1987904e339ebfe150102c8ca40f
Natalia Kondratieva <kondratieva07@gmail.com>
2022-08-20 17:47:34 +0300

    complete structure

---
+++ src/components/Profile.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const Profile = () => {
+    return (
+        <div className={'content'}>
+            <img
+                src={'https://cdn.pixabay.com/photo/2014/09/14/18/04/dandelion-445228__340.jpg'} alt={'photo'}/>
+            <div> ava+description</div>
+            <div>
+                my posts
+                <div>new posts</div>
+            </div>
+            <div>
+                post 1
+            </div>
+            <div>
+                post 2
+            </div>
+        </div>
+
+    )
+}
